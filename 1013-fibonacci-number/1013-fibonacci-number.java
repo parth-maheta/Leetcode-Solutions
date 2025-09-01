c1c0c1afca45f05e9a6f16685e957cbd0c3e1772
Parth Maheta <156274924+parth-maheta@users.noreply.github.com>
2025-09-01 18:33:31 +0530

    Time: 0 ms (100%), Space: 40.2 MB (81.1%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,17 @@
 class Solution {
     public int fib(int n) {
-       if(n<=1) return n;
-       return fib(n-1)+fib(n-2);
+       if(n==0) return 0;
+       else if(n==1) return 1;
+       
+        int first=0,second=1;
+        for(int i=0;i<n;i++){
+            int third=first + second;
+            first=second;
+            second=third;
+        }
+       return first;
+
+     
         
     }
 }
