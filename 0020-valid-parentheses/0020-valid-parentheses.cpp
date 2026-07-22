26e166531824648b73758b0086e9c5bc0a55dab8
Parth Chiragkumar Maheta <156274924+parth-maheta@users.noreply.github.com>
2026-07-22 22:56:59 +0530

    Time: 0 ms (100%), Space: 10 MB (9.15%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(char ch:s){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(ch==')' && st.top()=='('){
+                    st.pop();
+                }
+                else if(ch=='}' && st.top()=='{'){
+                    st.pop();
+                }
+                else if(ch==']' && st.top()=='['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+};
