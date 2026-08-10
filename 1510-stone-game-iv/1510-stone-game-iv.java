7ecc1511dfde1fafd92f1ead5594104b5693892f
Parth Chiragkumar Maheta <156274924+parth-maheta@users.noreply.github.com>
2026-08-10 13:16:03 +0530

    Time: 42 ms (26.37%), Space: 48.9 MB (46.74%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    int[] dp;
+
+    public boolean solve(int n) {
+
+    
+        if (n == 0) {
+            return false;
+        }
+
+       
+        if (dp[n] != -1) {
+            return dp[n] == 1;
+        }
+
+    
+        for (int i = 1; i * i <= n; i++) {
+
+            if (!solve(n - i * i)) {
+                dp[n] = 1;
+                return true;
+            }
+        }
+
+        dp[n] = 0;
+        return false;
+    }
+
+    public boolean winnerSquareGame(int n) {
+
+        dp = new int[n + 1];
+
+       
+        Arrays.fill(dp, -1);
+
+        return solve(n);
+    }
+}
